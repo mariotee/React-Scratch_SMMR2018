a962e1b13d9b4e3c633de1c3054caddb0a54561f
Mario Torres <mariotee10@gmail.com>
2018-06-28 11:45:36 -0500

    adding css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import blockstyle from "./someblock.css"
+import otherblockstyle from "./someotherblock.css"
+import spanstyle from "./somespan.css"
+import otherspanstyle from "./someotherspan.css"
+
 class Index extends React.Component
 {
   render()
   {
-    return <div>Hello React</div>
+    return(
+    <div>
+      <div className={blockstyle.mains}>Some Block</div>
+      <div className={otherblockstyle.mains}>Some Other Block</div>
+      <span className={spanstyle.mains}>Some Span</span> <span className={otherspanstyle.mains}>Some Other Span</span>
+    </div>
+    )
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const htmlPlugin = new HtmlWebPackPlugin(
+const htmlWebpackPlugin = new HtmlWebPackPlugin(
 {
   template: "./src/index.html",
   filename: "./index.html"
@@ -15,12 +15,31 @@ module.exports =
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use:
-        {
+        use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use:
+        [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options:
+            {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlWebpackPlugin]
 };
